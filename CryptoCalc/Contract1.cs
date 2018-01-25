5acb74bb9397cfacc99bfeaa98655b016fa4d9c4
Tom Andrews <tom.andrews@c5alliance.com>
2018-01-25 20:43:25 +0000

    Initial commit

--- CryptoCalc/Contract1.cs
+++ CryptoCalc/Contract1.cs
@@ -5,11 +5,66 @@ using System.Numerics;
 
 namespace CryptoCalc
 {
-    public class Contract1 : SmartContract
+    public class Calculator : SmartContract
     {
-        public static void Main()
+        public static int Main(string operation, int number1, int number2)
         {
-            Storage.Put(Storage.CurrentContext, "Hello", "World");
+            Runtime.Notify("Operation started: ", number1, number2);
+            int result = 0;
+
+            if (operation == "add" || operation == "+")
+            {
+                result = Add(number1, number2);
+            }
+
+            if (operation == "subtract" || operation == "-")
+            {
+                result = Subtract(number1, number2);
+            }
+
+            Runtime.Notify("Calculation finished", operation, result);
+            return result;
+        }
+
+
+        static private int Add(int a, int b)
+        {
+            int sum = a + b;
+
+            Runtime.Notify("Add operation invoked with", a, b);
+            Runtime.Notify("Add operation result", sum);
+
+            return sum;
+        }
+
+        static private int Subtract(int a, int b)
+        {
+            int sum = a - b;
+
+            Runtime.Notify("Subtract operation invoked with", a, b);
+            Runtime.Notify("Subtract operation result", sum);
+
+            return sum;
+        }
+
+        static private int Multiply(int a, int b)
+        {
+            int sum = a * b;
+
+            Runtime.Notify("Multiply operation invoked with", a, b);
+            Runtime.Notify("Multiply operation result", sum);
+
+            return sum;
+        }
+
+        static private int Divide(int a, int b)
+        {
+            int sum = a / b;
+
+            Runtime.Notify("Divide operation invoked with", a, b);
+            Runtime.Notify("Divide operation result", sum);
+
+            return sum;
         }
     }
 }
